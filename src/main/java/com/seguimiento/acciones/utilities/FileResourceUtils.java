2ab7aadd77b9414d9c5352fb14035f566f6d9630
RafaLega <rafael.pereztorrecilla@gmail.com>
2022-03-03 16:36:30 +0100

    modificada clase, se realizan los throws. closes #26

--- src/main/java/com/seguimiento/acciones/utilities/FileResourceUtils.java
+++ src/main/java/com/seguimiento/acciones/utilities/FileResourceUtils.java
@@ -1,23 +1,18 @@
 package com.seguimiento.acciones.utilities;
 
 import java.io.InputStream;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 
 public class FileResourceUtils {
 	
-	private static final Logger logger = LogManager.getLogger(FileResourceUtils.class);
 
 	public InputStream getFileFromResourceAsStream(String fileName) {
-		logger.debug("Llamada al metodo getFileFromResourceAsStream con parametro: {}",fileName);
 		ClassLoader classLoader = getClass().getClassLoader();
 		InputStream inputStream = classLoader.getResourceAsStream(fileName);
 		
 		if(inputStream == null) {
-			logger.error("Archivo no encontrado! {}", fileName);
 			throw new IllegalArgumentException("Archivo no encontrado! "+ fileName);
 		} else {
-			logger.debug("Devolucion del metodo getFileFromResourceAsStream con parametro: {}",inputStream.toString());
 			return inputStream;
 		}
 	}
